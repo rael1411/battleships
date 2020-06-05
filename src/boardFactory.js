9e246f5bf9859e9043e8c8b982504d4cec4e805c
Alessio <barrialex@gmail.com>
2020-06-05 17:41:41 +0200

    slight cleanup, refactoring

--- src/boardFactory.js
+++ src/boardFactory.js
@@ -6,37 +6,18 @@ function boardFactory() {
   for (let i = 0; i < BOARDSIZE * 10; i++) {
     state.push({ ship: false, hit: false });
   }
+  let ships = [];
   return {
     //initializes 10 by 10 array of an empty game board
     state: state,
     addShip(position, size, align) {
       //checks if the position can hold a ship
       // if true the ship gets added, otherwise returns false
-      if (align === "horiz") {
-        if (
-          Math.floor(position / 10) !== Math.floor((position + size) / 10) ||
-          position + size > 99
-        ) {
-          return false;
-        } else {
-          for (let i = 0; i < size; i++) {
-            if (this.state.ship === true) {
-              return false;
-            }
-          }
-        }
+      if (checkValidPosition(position, size, align, state) === false) {
+        return false;
+      } else {
         for (let i = 0; i < size; i++) {
           this.state[position + i].ship = true;
-        }
-      } else {
-        if (position + size * 10 > 99) {
-          return false;
-        } else {
-          for (i = 0; i < size; i++) {
-            if (this.state[position + 10 * i].ship === true) {
-              return false;
-            }
-          }
         }
         for (i = 0; i < size; i++) {
           this.state[position + 10 * i].ship = true;
@@ -49,5 +30,30 @@ function boardFactory() {
       }
     },
   };
+}
+
+function checkValidPosition(position, size, align, state) {
+  if (align === "horiz") {
+    if (
+      Math.floor(position / 10) !== Math.floor((position + size) / 10) ||
+      position + size > 99
+    ) {
+      return false;
+    }
+    for (let i = 0; i < size; i++) {
+      if (state.ship === true) {
+        return false;
+      }
+    }
+  }
+  if (position + size * 10 > 99) {
+    return false;
+  }
+  for (i = 0; i < size; i++) {
+    if (state[position + 10 * i].ship === true) {
+      return false;
+    }
+  }
+  return true;
 }
 module.exports = boardFactory;
